366ff2a5ec33eef897f2f365853a3d787a41bda5
Julien Carignan <julien.carignan@atelierfolklore.ca>
2016-03-07 03:02:08 -0500

    more

--- templates/header.php
+++ templates/header.php
@@ -3,17 +3,17 @@
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'main-nav']);
       endif;
       ?>
     </nav>
     <div class="right-content">
       <?php
       if (has_nav_menu('top_right')) :
-        wp_nav_menu(['theme_location' => 'top_right']);
+        wp_nav_menu(['theme_location' => 'top_right', 'menu_class' => 'mini-menu']);
       endif;
       if (has_nav_menu('social_medias')) :
-        wp_nav_menu(['theme_location' => 'social_medias']);
+        wp_nav_menu(['theme_location' => 'social_medias', 'menu_class' => 'social-medias']);
       endif;
       ?>
     </div>
